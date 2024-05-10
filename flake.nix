5f147ece2122d6e4cad91e75efdef94dc99b6fb5
Buster Daemon <bugsbunnygdi@pm.me>
2024-05-10 13:37:13 +0300

    Now it's flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        name = "E621 & Rule34 downloader";
+	buildInputs = with pkgs; [
+          go
+	  gotools
+	  golangci-lint
+	  gopls
+	  go-outline
+	  gopkgs
+	  delve
+	];
+      };
+    };
+  
+}
