3598eb79d68a8370e0f54eace70c7bd855305c16
Buster Daemon <bugsbunnygdi@pm.me>
2024-05-26 17:19:20 +0300

    Added user agent rotation

--- internal/download/download.go
+++ internal/download/download.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -62,6 +63,15 @@ func (d Download) DwPosts(p []parsers.Post) error {
 		Transport: &trs,
 		Timeout:   30 * time.Second,
 	}
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+		"Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+		"Mozilla/5.0 (Linux; U; Android-4.0.3; en-us; Galaxy Nexus Build/IML74K) AppleWebKit/535.7 (KHTML, like Gecko) CrMo/16.0.912.75 Mobile Safari/535.7",
+	}
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	fNameUrl := func(url string) string {
 		splUrl := strings.Split(url, "/")
 		return splUrl[len(splUrl)-1]
@@ -85,6 +95,12 @@ func (d Download) DwPosts(p []parsers.Post) error {
 				end = len(p)
 			}
 			for j := start; j < end; j++ {
+				if p[j].FileUrl == "" {
+					d.Logger.Println("File URL is empty")
+					totProgress.Add(1)
+					continue
+				}
+				rng.Seed(time.Now().UnixNano())
 				res := db.Where("hash = ?", p[j].Hash).First(&parsers.Post{
 					Hash: p[j].Hash,
 				}).Order("file_url DESC")
@@ -107,6 +123,8 @@ func (d Download) DwPosts(p []parsers.Post) error {
 					totProgress.Add(1)
 					continue
 				}
+
+				resp.Request.Header.Set("User-Agent", userAgents[rng.Intn(len(userAgents)-1)])
 				defer resp.Body.Close()
 				defer f.Close()
 
